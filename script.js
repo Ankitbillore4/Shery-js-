0940aefcbff8461eacb9073f6f4db653338c0be4
billoreankit66@gmail.com <billoreankit66@gmail.com>
2024-07-31 23:43:59 +0530

    new

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 Shery.mouseFollower({
-    
+
     skew: true,
     ease: "cubic-bezier(0.23, 1, 0.320, 1)",
     duration: 1,
-  });
-  
+});
 
-  Shery.makeMagnet(".navpart2 h3", {
-    
+
+Shery.makeMagnet(".navpart2 h3", {
+
     ease: "cubic-bezier(0.23, 1, 0.320, 1)",
     duration: 1,
-  });
+});
