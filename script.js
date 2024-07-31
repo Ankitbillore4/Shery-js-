cb457ed70778b9b0a9ce5dd1f694ce3d4a59a0ea
billoreankit66@gmail.com <billoreankit66@gmail.com>
2024-07-31 23:42:50 +0530

    changes

--- script.js
+++ script.js
@@ -4,3 +4,10 @@ Shery.mouseFollower({
     ease: "cubic-bezier(0.23, 1, 0.320, 1)",
     duration: 1,
   });
+  
+
+  Shery.makeMagnet(".navpart2 h3", {
+    
+    ease: "cubic-bezier(0.23, 1, 0.320, 1)",
+    duration: 1,
+  });
